53d09b03cf85425152bbe6fb53d7af18f6588926
ViniciusdePSouza <dev.souzavinicius@gmail.com>
2023-04-14 11:36:25 -0300

    Components: Groups card component created

--- itoddy-teams/src/screens/Groups/index.tsx
+++ itoddy-teams/src/screens/Groups/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Container } from './styles';
 import { Header } from '../../Components/Header';
+import { Highlight } from '../../Components/Highlight';
+import { GroupCard } from '../..//Components/GroupCard';
 
 export function Groups() {
+  function handleOnPress() {
+    console.log('chegando aqui')
+  }
+
   return (
     <Container >
       <Header showBackButton/>
+      <Highlight title="Turmas" subtitle="Jogue com a sua turma"/>
+      <GroupCard title="Los Atiradores"/>
+      <GroupCard title="Los Apostadores" onPress={handleOnPress}/>
     </Container>
   );
 }
